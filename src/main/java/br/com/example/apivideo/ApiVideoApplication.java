cce3e51428b07e6e538bc72d29186f9a59979b96
Axel <axelddlima@hotmail.com>
2023-10-02 11:24:51 -0300

    Primeiro commit, criação do projeto

---
+++ src/main/java/br/com/example/apivideo/ApiVideoApplication.java
@@ -0,0 +1,13 @@
+package br.com.example.apivideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVideoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVideoApplication.class, args);
+	}
+
+}
