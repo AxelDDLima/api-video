804d688f5f9b45cd70bd3f920513171f9052e0bd
Axel <axelddlima@hotmail.com>
2023-10-03 14:08:43 -0300

    Implementação do swagger

--- src/main/java/br/com/example/apivideo/ApiVideoApplication.java
+++ src/main/java/br/com/example/apivideo/ApiVideoApplication.java
@@ -3,7 +3,10 @@ package br.com.example.apivideo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
 @SpringBootApplication
+@OpenAPIDefinition
 public class ApiVideoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/example/apivideo/controller/UsersController.java
+++
@@ -1,8 +0,0 @@
-package br.com.example.apivideo.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class UsersController {
-
-}
